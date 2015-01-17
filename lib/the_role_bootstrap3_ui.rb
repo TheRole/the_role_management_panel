c4fe5ae525bca6bae0d2060def6d6dadcc84a5b1
Ilya N. Zykin <zykin-ilya@ya.ru>
2015-01-17 23:50:13 +0300

    files_moved

--- lib/the_role_bootstrap3_ui.rb
+++ lib/the_role_bootstrap3_ui.rb
@@ -4,3 +4,8 @@ require_relative "the_role_bootstrap3_ui/version"
 module TheRoleBootstrap3Ui
   class Engine < Rails::Engine; end
 end
+
+_root_ = File.expand_path('../../',  __FILE__)
+
+# # Loading of concerns
+require "#{_root_}/config/routes.rb"
