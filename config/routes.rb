fa07f3789004d800884343744f915fbfc0c13c6b
Ilya N. Zykin <zykin-ilya@ya.ru>
2015-01-30 00:05:59 +0300

    routes upd

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
-module TheRole
+module TheRoleBootstrap3Ui
+  # TheRoleBootstrap3Ui::Routes.mixin(self)
   class Routes
     def self.mixin mapper
-      mapper.resources :roles, :except => :show do
+      mapper.resources :roles, except: :show do
         mapper.collection do
           mapper.get   :export
           mapper.patch :import
         end
 
         mapper.member do
-          mapper.get   'role_export'
-          mapper.patch 'change'
+          mapper.get   :role_export
+          mapper.patch :change
         end
 
-        mapper.resources :sections, :controller => :role_sections, :only => :none do
+        mapper.resources :sections, controller: :role_sections, only: :none do
           mapper.collection do
             mapper.post :create
             mapper.post :create_rule
@@ -27,25 +28,24 @@ module TheRole
           end
         end
       end
-    end
-  end
 
-  class AdminRoutes
-    def call mapper, options = {}
-      mapper.resources :roles, :except => :show do
-        mapper.patch 'change', on: :member
-        mapper.resources :sections, :controller => :role_sections, :only => :none do
-          mapper.collection do
-            mapper.post :create
-            mapper.post :create_rule
-          end
+      mapper.namespace :admin do
+        mapper.resources :roles, except: :show do
+          mapper.patch :change, on: :member
 
-          mapper.member do
-            mapper.put :rule_on
-            mapper.put :rule_off
+          mapper.resources :sections, controller: :role_sections, only: :none do
+            mapper.collection do
+              mapper.post :create
+              mapper.post :create_rule
+            end
 
-            mapper.delete :destroy
-            mapper.delete :destroy_rule
+            mapper.member do
+              mapper.put :rule_on
+              mapper.put :rule_off
+
+              mapper.delete :destroy
+              mapper.delete :destroy_rule
+            end
           end
         end
       end
